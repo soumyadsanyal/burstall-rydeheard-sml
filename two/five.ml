9fa4de7ee860e19e99e2023d2c393e54ad03ae2e
soumyadsanyal <soumya@soumyadsanyal.com>
2017-12-24 00:24:37 -0500

    five done

--- two/five.ml
+++ two/five.ml
@@ -8,11 +8,35 @@ fun indicesWithStart(l:('a list), start:int) = case l of
 
 val indices = fn (l:'a list) => indicesWithStart(l,0)
 
+fun pow(b:int, exp:int):int = case exp of
+    0 => 1
+    | WILD => b*pow(b,exp-1)
+
+fun powlist(b:int, l:(int list)):(int list) = map((fn y => pow(b,y)))(l)
+
+fun innerprod(l:(int list), l':(int list)):int = case (l,l') of
+    (_, []) => 0
+    | ([], _) => 0
+    | (x::xs, y::ys) => x*y + innerprod(xs,ys)
+
+exception empty_list
+
+fun append(s:('a list), t:('a list)):('a list) = hd(s)::append(tl(s),t) handle Empty => t
+
 (* definitions*)
 
 fun maxList(l:(int list)):int = foldl (fn (x,acc) => if (acc>x) then acc else x) (hd l) l
 
 fun sumList(l:(int list)):int = foldl (fn (x,acc) => acc+x) 0 l
+
+fun poly(coeffs:(int list), b:int):int = innerprod(coeffs,powlist(b, range(0,length(coeffs)-1)))
+
+fun revList(l:('a list)):('a list) = foldl (fn (x,acc) => append([x], acc)) [] l
+
+fun mapList(f:'a -> 'b, l:('a list)) = case l of [] => [] | (b::bs) => (f(b))::(mapList(f, bs))
+
+fun myfoldl = foldl
+
 
 
 
